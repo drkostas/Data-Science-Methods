da813887d736b99a062d9b371ab7df64a24fa12d
drkostas <georgiou.kostas94@gmail.com>
2021-02-07 22:02:06 -0500

    Made the options available from argparse. Added sh file with default options

--- isaac/parallel_bench.py
+++ isaac/parallel_bench.py
@@ -6,21 +6,6 @@ import threading
 from concurrent import futures
 
 
-def timeit(method):
-    def timed(*args, **kw):
-        ts = time.time()
-        result = method(*args, **kw)
-        te = time.time()
-        if 'log_time' in kw:
-            name = kw.get('log_name', method.__name__.upper())
-            kw['log_time'][name] = int((te - ts) * 1000)
-        else:
-            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
-        return result
-
-    return timed
-
-
 class BenchTests:
     def __init__(self, max_float, loops):
         random.seed(2)
@@ -54,6 +39,45 @@ class BenchTests:
                 self.third_list.append(self.second_list.pop())
             except IndexError as ie:
                 pass
+
+
+def _argparser():
+    parser = argparse.ArgumentParser(
+        description='Parallel Processing Benchmarking.',
+        add_help=False)
+    # Required Args
+    required_arguments = parser.add_argument_group('Required Arguments')
+    choices = ['math_calc', 'fill_and_empty_list', 'all']
+    required_arguments.add_argument('-t', '--tests-to-run', choices=choices,
+                                    required=True,
+                                    help='Which tests to run.')
+    required_arguments.add_argument('-n', '--num-threads-processes', action='append',
+                                    help="Number of the threads and processes to use. Can add many.")
+    required_arguments.add_argument('-m', '--max-float', action='append',
+                                    help="Max float to use on each test. Add one arg for each test.")
+    required_arguments.add_argument('-l', '--num-loops', action='append',
+                                    help="Number of loops to do on each test. Add one arg for each test.")
+    # Optional args
+    optional = parser.add_argument_group('Optional Arguments')
+    optional.add_argument("-h", "--help", action="help",
+                          help="When adding -m and -l arguments, add them in the this order: %s" % choices[:-1])
+
+    return parser.parse_args()
+
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        return result
+
+    return timed
 
 
 @timeit
@@ -94,57 +118,50 @@ def run_with_concurrent(func, num_threads, *args):
         [executor.submit(func, *args) for _ in range(num_threads)]
 
 
-def _argparser():
-    parser = argparse.ArgumentParser(
-        description='Parallel Processing Benchmarking.',
-        add_help=False)
-    # Required Args
-    required_arguments = parser.add_argument_group('Required Arguments')
-    required_arguments.add_argument('-t', '--tests-to-run', choices=['math_calc', 'fill_and_empty_list', 'all'],
-                                    required=True,
-                                    help='Which tests to run.')
-    required_arguments.add_argument('-l', '--log', help="Name of the output log file")
-    # Optional args
-    optional = parser.add_argument_group('Optional Arguments')
-    optional.add_argument("-h", "--help", action="help", help="Show this help message and exit")
-
-    return parser.parse_args()
-
-
 def main():
     # Initializing
     args = _argparser()
-    test_proc_threads = [1, 2, 4, 8]
-    if args.tests_to_run in ['math_calc', 'all']:
+    test_proc_threads = [int(num) for num in args.num_threads_processes]
+    if args.tests_to_run == 'all':
+        if len(args.max_float) + len(args.num_loops) < 4:
+            raise Exception(
+                "You should add the -t and -l options, one time for each test, with the order specified in help.")
+    else:
+        if len(args.max_float) + len(args.num_loops) < 2:
+            raise Exception("You should add the -t and -l options. For more run with -h option.")
 
-        print("Starting benchmark for `math_calc`..")
+    max_float = [float(num) for num in args.max_float]
+    num_loops = [int(num) for num in args.num_loops]
+
+    if args.tests_to_run in ['math_calc', 'all']:
+        print("\n\nStarting benchmark for `math_calc`..")
         for num in test_proc_threads:
             # Multiprocessing
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[0], loops=num_loops[0])
             print("\nStarting with %s processes:" % num)
             run_with_multiprocess(math_ops.math_calc, num)
             # Threading
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[0], loops=num_loops[0])
             print("Starting with %s threads:" % num)
             run_with_threads(math_ops.math_calc, num)
             # Threading with concurrent
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[0], loops=num_loops[0])
             print("Starting with %s `concurrent` threads:" % num)
             run_with_concurrent(math_ops.math_calc, num)
 
     if args.tests_to_run in ['fill_and_empty_list', 'all']:
-        print("Starting benchmark for `fill_and_empty_list`..")
+        print("\n\nStarting benchmark for `fill_and_empty_list`..")
         for num in test_proc_threads:
             # Multiprocessing
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[-1], loops=num_loops[-1])
             print("\nStarting with %s processes:" % num)
             run_with_multiprocess(math_ops.fill_and_empty_list, num)
             # Threading
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[-1], loops=num_loops[-1])
             print("Starting with %s threads:" % num)
             run_with_threads(math_ops.fill_and_empty_list, num)
             # Threading with concurrent
-            math_ops = BenchTests(max_float=1.5, loops=10 ** 6)
+            math_ops = BenchTests(max_float=max_float[-1], loops=num_loops[-1])
             print("Starting with %s `concurrent` threads:" % num)
             run_with_concurrent(math_ops.fill_and_empty_list, num)
 
