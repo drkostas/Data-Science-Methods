b3f5a054d4f52aa6c248c3d15d160699f2b05339
drkostas <georgiou.kostas94@gmail.com>
2021-02-18 17:47:05 -0500

    Remove `ls` command and add comments to `run.sh` #3

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Get the absolute real path of the script and cd one directory up
+# This is necessary for working even from inside symlinks
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH/..
-ls
+# This the default conda env that `make install` creates
 CONDA_BIN="~/anaconda3/envs/dse512_playground/bin/"
+# Construct the command and run it with `eval`
 command="${CONDA_BIN}python assignment1/assignment1.py -c confs/assignment1.yml -l logs/assignment1.log"
 eval $command
