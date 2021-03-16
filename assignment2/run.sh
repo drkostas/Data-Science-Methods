30996ea0d401266e14dabc6642f955ba917899bf
drkostas <georgiou.kostas94@gmail.com>
2021-03-16 01:43:15 -0400

    Update the command that finds the real dir in run script #6

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-real_path=$(cd -P "." && pwd)
+real_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
 qsub ${real_path}/kmeans_vectorized_iris_nodes1ppn1.pbs
 qsub ${real_path}/kmeans_distributed_iris_nodes2ppn10.pbs
