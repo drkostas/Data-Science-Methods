89f78887f4cd65961f6fa5c252c1e681244e94c6
drkostas <georgiou.kostas94@gmail.com>
2021-03-16 01:41:19 -0400

    Update run script #6

--- assignment2/run.sh
+++ assignment2/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-qsub kmeans_vectorized_iris_nodes1ppn1.pbs
-qsub kmeans_distributed_iris_nodes2ppn10.pbs
-qsub kmeans_vectorized_tcga_nodes1ppn1.pbs
-qsub kmeans_distributed_tcga_nodes2ppn10.pbs
+real_path=$(cd -P "." && pwd)
+
+qsub ${real_path}/kmeans_vectorized_iris_nodes1ppn1.pbs
+qsub ${real_path}/kmeans_distributed_iris_nodes2ppn10.pbs
+qsub ${real_path}/kmeans_vectorized_tcga_nodes1ppn1.pbs
+qsub ${real_path}/kmeans_distributed_tcga_nodes2ppn10.pbs
