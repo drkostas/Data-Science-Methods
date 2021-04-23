6534eee9d8a01c9383caf2305e6777304d7f3580
drkostas <georgiou.kostas94@gmail.com>
2021-04-23 15:13:02 -0400

    I think everything is fixed #10

--- assignment4/run.sh
+++ assignment4/run.sh
@@ -2,9 +2,9 @@
 
 real_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
-#qsub ${real_path}/mnist_with_lenet5_non_parallel_1.pbs
-#qsub ${real_path}/mnist_with_lenet5_parallel_1.pbs
-#qsub ${real_path}/mnist_with_lenet5_parallel_2.pbs
+qsub ${real_path}/mnist_with_lenet5_non_parallel_1.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_1.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_2.pbs
 qsub ${real_path}/mnist_with_lenet5_parallel_4.pbs
 qsub ${real_path}/mnist_with_lenet5_parallel_8.pbs
 
