9677f878c96fabb9768586ebd13796e927085ae9
drkostas <georgiou.kostas94@gmail.com>
2021-04-22 22:08:54 -0400

    Created the pbs scripts and script that launches them #10

---
+++ assignment4/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+real_path="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+
+qsub ${real_path}/mnist_with_lenet5_non_parallel_1.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_1.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_2.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_3.pbs
+qsub ${real_path}/mnist_with_lenet5_parallel_8.pbs
